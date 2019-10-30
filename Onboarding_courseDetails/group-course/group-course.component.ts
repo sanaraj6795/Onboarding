49fb28ab7a2372b06d0c26f5accdf38e089c37d3
sanaraj6795 <56729864+sanaraj6795@users.noreply.github.com>
2019-10-30 15:58:15 +0530

    Add files via upload

--- Onboarding_courseDetails/group-course/group-course.component.ts
+++ Onboarding_courseDetails/group-course/group-course.component.ts
@@ -5,6 +5,7 @@ import { IUserGroup} from '../../model/userGroup';
 import { IGroupCourse } from '../../model/groupCourse';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { AddCourseDialogueComponent } from '../add-course-dialogue/add-course-dialogue.component';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-group-course',
@@ -25,12 +26,35 @@ export class GroupCourseComponent implements OnInit {
   roleType:Array<string>=['Programmer Analyst Trainee','program Analyst','Associate','Senior Associate'];
   startDate:Date;
   incrementDate:Date;
-  constructor(private userGroupService: UserGroupService,public dialog: MatDialog) { }
+
+  courseForm: FormGroup;
+  submitted = false;
+  constructor(private userGroupService: UserGroupService,public dialog: MatDialog, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
   }
-
+  
+    createForm() {
+    this.courseForm = this.formBuilder.group({
+      associateId:['',Validators.required],
+      role:['',Validators.required],
+      startDate:['',Validators.required]
+    });
+    
+  }
+ 
+ onSubmit(){
+   console.log(this.courseForm.get('associateId').value);
+   this.submitted=true;
+    if (this.courseForm.invalid) {
+      return;
+    }
+ }
+ get f() { return this.courseForm.controls; }
+ 
   addAssociateId(){
+    this.associateId=this.courseForm.get('associateId').value;
     if(this.associateId!=null){
     this.associateIdList.push(this.associateId);
     this.associateId=null;
@@ -44,16 +68,27 @@ export class GroupCourseComponent implements OnInit {
   }
 
   generateClicked(){
-  this.getCourseListDetails();
-  if(this.groupCourseCount!=0 && this.startDate != null){
+  this.submitted=true;
+    if (this.courseForm.invalid) {
+      return;
+    }
+    else{
+    this.getCourseListDetails();
+    if(this.groupCourseCount!=0 && this.startDate != null){
     this.isGenerateClicked=true;
     this.isGenerateDisabled=true;
+  }
   }
   } 
      
   clearData(){
   this.clearAll();
-  this.startDate=null;
+  this.courseForm.setValue({
+      associateId:this.courseForm.get('associateId').value,
+      role:"",
+      startDate:""
+    });
+  this.submitted = false;
   this.courseDetail=null;
   }
 
@@ -64,9 +99,11 @@ export class GroupCourseComponent implements OnInit {
   }
 
    private getCourseListDetails() {
+     this.startDate=this.courseForm.get('startDate').value;
      this.incrementDate=this.startDate;
      if(this.startDate != null){
-       this.userGroupService.getCourseList(this.role).subscribe(model => {
+       this.userGroupService.getCourseList(this.courseForm.get('role').value).subscribe(model => {
+         console.log(this.courseForm.get('role').value,this.courseForm.get('startDate').value);
             this.courseDetail = model
         });
       this.courselist(this.courseDetail.courseList);  
@@ -75,10 +112,10 @@ export class GroupCourseComponent implements OnInit {
 
  private courselist(courseList  : ICourseDetail[]){
   let i=1,j=1;
+  this.getWeekDays(this.incrementDate);
   const uniquevalue = Array.from(new Set(courseList.map(obj => obj.courseType)));
         uniquevalue.forEach(x => {
         courseList.forEach(y=>{
-          console.log(courseList);
         if(x == y.courseType){
         let data={
           day:i,  
@@ -90,7 +127,7 @@ export class GroupCourseComponent implements OnInit {
         this.groupCourse.push(data);
         let dt=new Date(this.incrementDate);
         dt.setDate(dt.getDate()+1);
-        this.incrementDate=dt;
+        this.getWeekDays(dt);
         if(i%5==0){
           j++;
         }
@@ -100,10 +137,20 @@ export class GroupCourseComponent implements OnInit {
       });
       this.groupCourseCount=this.groupCourse.length;
    }
-   
+
+   getWeekDays(newIncrementDate : Date){
+     let dt=new Date(newIncrementDate);
+      if(dt.getDay()==6){
+        dt.setDate(dt.getDate()+2);
+      }else if(dt.getDay() == 0){
+        dt.setDate(dt.getDate()+1);
+      }
+      this.incrementDate=dt;
+   }
+
    openDialog(): void {
-    const dialogRef = this.dialog.open(AddCourseDialogueComponent, {
-      width:'auto',
+    const dialogRef = this.dialog.open(AddCourseDialogueComponent,{
+      width:"auto"
     });
     dialogRef.afterClosed().subscribe(result => {
       if(result!=null){
@@ -112,13 +159,10 @@ export class GroupCourseComponent implements OnInit {
       result.forEach(e =>{
       this.courseDetail.courseList.push(e);
      });
+     this.incrementDate=this.startDate;
      this.courselist(this.courseDetail.courseList);
      this.isGenerateClicked=true;
      this.isGenerateDisabled=true;
-      //console.log(result);
-      //console.log(this.courseDetail.courseList);
-      //console.log('The dialog was closed');
-      this.incrementDate=this.startDate;
     }
     });
   }
